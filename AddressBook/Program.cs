299b23ecc7a032eea2bbd56b5c1424ba199b2722
pkr187 <pankajroman187@gmail.com>
2022-11-03 00:10:32 +0530

    [pankaj] Add:AddressBookSystem

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -29,7 +29,7 @@
             contact.zipcode = Convert.ToInt32(Console.ReadLine());
         }
 
-        public static void CreatingContacts()
+        public static void CreatingContacts(List<Contact> contacts)
         {
             Console.WriteLine("Do you want to add new contact press 1 or press 2 to cancle.");
             int num = Convert.ToInt32(Console.ReadLine());
@@ -39,23 +39,23 @@
             {
                 Contact contact = new Contact();
                 FillingDetails(contact);
-                contactList.Add(contact);
+                contacts.Add(contact);
 
                 Console.WriteLine("Do you want to add anoter contact then press 1 or press 2 for exit ");
                 num = Convert.ToInt32(Console.ReadLine());
             }
             Console.WriteLine("=============================================================");
-            Console.WriteLine("Total number of contact in address book:" + contactList.Count);
+            Console.WriteLine("Total number of contact in address book:" + contacts.Count);
         }
 
-        public static void DisplayContacts()
+        public static void DisplayContacts(List<Contact> contacts)
         {
             //print contacts
 
             Console.WriteLine("=============================================================");
             Console.WriteLine("Current contacts in adress book:");
 
-            foreach (Contact contact in contactList)
+            foreach (Contact contact in contacts)
             {
                 Console.WriteLine(contact.firstName);
             }
@@ -63,7 +63,7 @@
 
         }
 
-        public static void EditContacts()
+        public static void EditContacts(List<Contact> contacts)
         {
             Console.WriteLine("Do you want to edit contact details then press 1 or pres 2 for continue: ");
             int num = Convert.ToInt32(Console.ReadLine());
@@ -72,15 +72,15 @@
                 Console.WriteLine("Enter first name to edit details: ");
                 string firstName = Console.ReadLine();
                 bool found = false;
-                for (int i = 0; i < contactList.Count; i++)
+                for (int i = 0; i < contacts.Count; i++)
                 {
 
-                    if (contactList[i].firstName == firstName)
+                    if (contacts[i].firstName == firstName)
                     {
                         found = true;  //found the contact
 
                         //now editing...
-                        FillingDetails(contactList[i]);
+                        FillingDetails(contacts[i]);
                         break;
 
                     }
@@ -88,33 +88,33 @@
                 if (!found)
                     Console.WriteLine("the contact with given person {0} is not in address book", firstName);
                 //print contacts
-                DisplayContacts();
+                DisplayContacts(contacts);
                 Console.WriteLine("Do you want to edit contact press 1 to edit or press 2 to cancle.");
                 num = Convert.ToInt32(Console.ReadLine());
             }//while loop end
 
         }
 
-        public static void DeleteContacts()
+        public static void DeleteContacts(List<Contact> contacts)
         {
             //deleting contact
             Console.WriteLine("Do you want to delete contact press 1 to delete or press 2 to cancle.");
             int num = Convert.ToInt32(Console.ReadLine());
 
-            while (num == 1 && contactList.Count > 0)
+            while (num == 1 && contacts.Count > 0)
             {
                 Console.WriteLine("Enter contact First name");
                 string firstName = Console.ReadLine();
 
                 bool found = false;
-                for (int i = 0; i < contactList.Count; i++)
+                for (int i = 0; i < contacts.Count; i++)
                 {
 
-                    if (contactList[i].firstName == firstName)
+                    if (contacts[i].firstName == firstName)
                     {
                         found = true;  //found the contact
 
-                        contactList.RemoveAt(i);
+                        contacts.RemoveAt(i);
                         break;
 
                     }
@@ -122,29 +122,68 @@
 
                 if (found)
                 {
-                    if (contactList.Count == 0) //if size 0 nothing to delete further
+                    if (contacts.Count == 0) //if size 0 nothing to delete further
                         break;
                 }
                 else
                     Console.WriteLine("the contact with given person '{0}' is not in address book", firstName);
                 //displaying contacts
-                DisplayContacts();
+                DisplayContacts(contacts);
                 Console.WriteLine("Do you want to delete contact press 1 to delete or press 2 to cancle.");
                 num = Convert.ToInt32(Console.ReadLine());
 
             }//while end
         }
 
-        //creating list of contact
-        public static List<Contact> contactList = new List<Contact>();
+        public static void DisplayDictionary()
+        {
+            Console.WriteLine("Diplay current data in addressbook: ");
+            foreach (KeyValuePair<string, List<Contact>> obj in addressBookSystem)
+            {
+                Console.WriteLine("Displaying contacts of adressbook {0}", obj.Key);
+                DisplayContacts(obj.Value);
+                Console.WriteLine("===============================================");
+            }
+        }
+        public static void CreateAddresBook()
+        {
+            Console.WriteLine("Do you want to create new AddressBook press 1 for yes or 2 for no:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+
+            while (num == 1)
+            {
+                Console.WriteLine("Please enter a name of addressbook:");
+                string name = Console.ReadLine();
+                //creating list of contact
+                List<Contact> addressBook = new List<Contact>();
+                addressBookSystem.Add(name, addressBook);
+
+                CreatingContacts(addressBook);
+
+                if (addressBook.Count > 0)
+                {
+                    EditContacts(addressBook);
+                    DeleteContacts(addressBook);
+                }
+                DisplayDictionary();
+
+                Console.WriteLine("Do you want to create another addressbook press 1 or press 2 for exit:");
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+
+        public static Dictionary<string, List<Contact>> addressBookSystem = new Dictionary<string, List<Contact>>();
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Sytem.");
 
-            CreatingContacts();
-            DisplayContacts();
-            EditContacts();
-            DeleteContacts();
+            CreateAddresBook();
+            DisplayDictionary();
+            //DisplayContacts();
+            //EditContacts();
+            //DeleteContacts();            
         }
     }
 }
